515711b117f2820a3480b1f698666bec89274d60
Chaichontat <chaichontat.s@duke.edu>
2021-04-07 20:21:11 -0400

    Add online function.

--- models/rbfn.py
+++ models/rbfn.py
@@ -10,17 +10,19 @@ Takes ~5 ms to run per step on a 4 GHz Coffee Lake CPU.
 """
 
 from functools import partial
-from typing import Callable
+from typing import Callable, Optional
 
 import jax.numpy as np
 from jax import jit
 from jax.api import value_and_grad
 from jax.interpreters.xla import DeviceArray
+from jax.ops import index, index_update
 
 
 #%%
 class RBFN:
-    def __init__(self, ker: Callable, params, optimizer: tuple[Callable, ...]) -> None:
+    # When subclassing, don't forget class name in self._mse. Static method limitation.
+    def __init__(self, ker: Callable, params, optimizer: tuple[Callable, ...], window=100) -> None:
         assert {"W", "τ", "c", "σ"} <= params.keys()
         assert params["W"].shape == params["c"].shape
         assert params["W"].shape[0] == params["σ"].size
@@ -32,10 +34,13 @@ class RBFN:
 
         self.ker = ker
         self._obj = self._mse_vgrad = jit(value_and_grad(self._mse, argnums=2), static_argnums=0)
-        self.i = 0
+        self.t = 1
+        self.window = window
+        self.mask = np.zeros((self.window - 1, 1))  # For online training. -1 since this is multiplied with x[:-1].
 
     @property
     def params(self):
+        """Params are in in PyTree in self.opt_state for optimizer."""
         return self.get_params(self.opt_state)
 
     def g(self, x):
@@ -44,20 +49,36 @@ class RBFN:
     def obj(self, x):
         return self._obj(self.ker, x, self.params)
 
-    def step(self, x, loop=3):
+    def step(self, x, loop=3, _mask=None):
         for _ in range(loop):
-            value, grads = self._obj(self.ker, x, self.params)
-            self.opt_state = self.opt_update(self.i, grads, self.opt_state)
-        self.i += 1
+            value, grads = self._obj(self.ker, x, self.params, mask=_mask)
+            self.opt_state = self.opt_update(self.t, grads, self.opt_state)
+        self.t += 1
         return value
+    
+    def step_online(self, x, loop=3):
+        """
+        Assuming the role for splitting up data for online learning for now.
+        Pad for t=0...self.window.
+        """
+        self.mask = index_update(self.mask, self.t - 1, 1.)
+        if self.t < self.window:
+            z = index_update(np.zeros((self.window, x.shape[1])), index[:self.t + 1, :], x[:self.t + 1, :])
+        else:
+            z = x[self.t: self.t + self.window]
+        return self.step(z, loop, self.mask)
 
     @staticmethod
     @partial(jit, static_argnums=0)
-    def _g(ker, x, p):
+    def _g(ker, x, p: dict):
         W, τ, c, σ = p["W"], p["τ"], p["c"], p["σ"]
         return ker(x, c, σ) @ W - np.exp(-(τ ** 2)) * x  # (4)
 
     @staticmethod
-    def _mse(ker: Callable, x: DeviceArray, p: dict[str, DeviceArray]):
-        "||g(x_{t-1}) + x_{t-1} - x_t||²"
-        return np.mean(np.square(RBFN._g(ker, x[:-1], p) + x[:-1] - x[1:]))
+    def _mse(ker: Callable, x: DeviceArray, p: dict[str, DeviceArray], mask: Optional[DeviceArray]=None):
+        """||g(x_{t-1}) + x_{t-1} - x_t||²"""
+        if mask is None:
+            return np.mean(np.square(RBFN._g(ker, x[:-1], p) + x[:-1] - x[1:]))
+        return np.sum(np.square(RBFN._g(ker, x[:-1], p) + x[:-1] - x[1:]) * mask) / np.sum(mask)
+
+# %%
