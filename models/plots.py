6ea93941ca92ea8429cb77e4e914d044dd9ab744
Na Young Jun <nayoung.jun@duke.edu>
2021-05-25 19:46:12 -0700

    script to generate log prob comparison plots

--- models/plots.py
+++ models/plots.py
@@ -11,23 +11,36 @@ def ewma(data, com):
 
 
 def saveplot():
-    data = np.load(sys.argv[1])
+    zp2016 = np.load(sys.argv[1])
+    vjf = np.load(sys.argv[2])
+    bwrap = np.load(sys.argv[3])
+
     plt.ioff()
-    plt.plot(data, color='grey', alpha=0.2)
+    nn = vjf.shape[0]
+
+    plt.plot(np.linspace(100, nn - 1, nn - 100), zp2016, color='red', alpha=0.2)
+    plt.plot(vjf, color='grey', alpha=0.2)
+    plt.plot(bwrap, color='lightblue', alpha=0.2)
 
     ax = plt.gca()
-    ax.plot(ewma(data, 100), color='black')
+    ax.plot(np.linspace(100, nn - 1, nn - 100), ewma(zp2016, 100), color='red', label='ZP2016')
+    ax.plot(ewma(vjf, 100), color='black', label='vjf')
+    ax.plot(ewma(bwrap, 100), color='blue', label='bubblewrap')
     ax.ticklabel_format(useOffset=False, style='plain')
+
     ax.set_xlabel('time step')
     ax.set_ylabel('log probability')
     ax.set_title('predictive log probability at each time step')
+    ax.legend()
 
-    plt.savefig(f"{os.path.splitext(os.path.basename(sys.argv[1]))[0]}.pdf")
-    # ax.set_ylim([-30,0])
+    ax.set_ylim([sys.argv[4], sys.argv[5]])
+
+    plt.savefig(f"{os.path.splitext(os.path.basename(sys.argv[3]))[0]}.svg")
+
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2:
+    if len(sys.argv) == 6:
         saveplot()
     else:
-        print(f"usage: python {sys.argv[0]} (filename)", file=sys.stderr)
+        print(f"usage: python {sys.argv[0]} (ZP2016-filename) (VJF-filename) (bubblewarp-filename) (ylim-min) (ylim max)", file=sys.stderr)
