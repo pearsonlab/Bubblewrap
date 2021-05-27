ca51f54d77f2b7b64a6a0e9aee07cdbb320cd0f8
pgupta29 <pranjal.gupta@duke.edu>
2021-05-27 08:36:52 -0400

    adding single trajectory plots, widefield script

--- scripts/run_jpca.py
+++ scripts/run_jpca.py
@@ -1,258 +1,314 @@
+#%%
 import time
+from math import atan2
 import numpy as np
-
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pylab as plt
-from matplotlib.patches import Ellipse
-from mpl_toolkits.mplot3d import Axes3D
-
+from numpy.lib.ufunclike import _fix_and_maybe_deprecate_out_named_y
+import pandas as pd
 from scipy.integrate import solve_ivp
 from scipy.stats import multivariate_normal as mvn
 
-from field.gqds import GQDS
-from datagen.diffeq import vanderpol, lorenz
+import matplotlib
+import matplotlib.pylab as plt
+import matplotlib.cm as cm
+from matplotlib.patches import Ellipse
+
+from field.gqds import GQDS, get_fullSigma
+from datagen.diffeq import vanderpol
 from field.utils import center_mass
+from datagen.plots import draw_bubbles, draw_graph, plot_scatter_connected
 from datagen import plots
 
-from math import atan2, floor
+import jPCA
+from jPCA.util import load_churchland_data, plot_projections
 
-s = np.load('jpca_reduced.npy')
-data = s.reshape((s.shape[0]*s.shape[1], s.shape[2]))
-# data = np.vstack((data, data, data, data))
-T = data.shape[0]
-d = data.shape[1]
+from proSVD import proSVD
 
-## parameters
+import networkx as nx
 
-# d = 6
-N = 64 #3**6 #num_d**d
-step = 8e-2
+#%% load jPCA data
+# data = projected
+path = "/hdd/pgupta/"
+dataloc = path + 'jpca_full.npy'
+dataloc_jpcaed = path + 'jpca_reduced.npy'
+data = np.load(dataloc)
+data_jpcaed = np.load(dataloc_jpcaed)
+# trials, timesteps, n = data.shape
+# (trials, window, components) = data_jpcaed.shape
+
+timesteps = 21
+# data = data[:, :timesteps, :]
+# data = data.reshape((trials*timesteps, n))
+# data_jpcaed = data_jpcaed.reshape((trials*window, components))
+
+# repeats = 2
+# total = timesteps * 108 #int(data.shape[0] / 2)
+# data = data[:total, :]
+data = data_jpcaed[:, :].T
+# data = np.fliplr(data, axis=0)
+# # repeating data
+# for i in range(repeats):
+    # data = np.append(data, data, axis=0)
+#%%
+def plot_jPCA_trials(t, timesteps, data, ax=None, alpha=.1, color='k'):
+    full_trials = np.floor( t / timesteps).astype('int')
+
+    ind = 0
+    # colors = cm.rainbow(np.linspace(0,1,full_trials))
+    for j in range(full_trials):
+        ax.plot(data[ind:ind+timesteps, 0], data[ind:ind+timesteps, 1], 
+                alpha=alpha, color=color, zorder=10) #colors[j])
+        # ax.scatter(data[ind, 0], data[ind, 1], alpha=0.1)
+        ind += timesteps
+
+def ewma(data, com):
+    return np.array(pd.DataFrame(data=dict(data=data)).ewm(com).mean()['data'])
+
+def get_fullSigma(L):
+    inv = np.linalg.inv(L)
+    return inv.T @ inv
+
+
+#%% params
+make_movie = False
+
+# general params
+dt = 0.1
+M = timesteps*4 # 4 trials
+T = data.shape[0] - M # iters
+t = np.linspace(0, dt*T, T)
+k = 6
+
+# ssSVD params
+ssSVD = False
+trueSVD = False
+if ssSVD:
+    l = 1 # should be able to handle this
+    l1 = k
+    decay = 1
+    pro = proSVD(k=k, decay_alpha=decay, 
+                 trueSVD=trueSVD, history=0)
+
+# bubblewrap params
+d = k
+num_d = 3
+N = 100
 lam = 1e-3
-nu = 1e-3 
-eps = 1e-3
+nu = 1e-5
 
-M = 60
-# T = 300    #500 + M
-P = 0 
-
-t_wait = 1 
+step = 9e-2
+# sigma_scale = 1e3
 B_thresh = -10
 n_thresh = 5e-4
+eps = 0
 
-mu_diff = 5e-2
+P = 0 
+t_wait = 1 # breadcrumbing
+gq = GQDS(N, num_d, d, step=step, lam=lam, eps=eps, M=M,
+          nu=nu, t_wait=t_wait, n_thresh=n_thresh, B_thresh=B_thresh)
 
-batch = False
-batch_size = 1 
-
-gq = GQDS(N, d, step=step, lam=lam, M=M, eps=eps, nu=nu, t_wait=t_wait, B_thresh=B_thresh, n_thresh=n_thresh, batch=batch, batch_size=batch_size, mu_diff=mu_diff)
-
-## initialize things
-for i in np.arange(0,M): #,batch_size):
-    gq.observe(data[i]) #:i+batch_size])
+##%% init and run
+if ssSVD:
+    # init with just regular svd
+    pro.initialize(data[:max(k, M), :].T)
+    data_red = data[:max(k, M)] @ pro.Q
+    for i in np.arange(M):
+        gq.observe(data_red[i])
+else:
+    for i in np.arange(M):
+        gq.observe(data[i])
+        
 
 gq.init_nodes()
 print('Nodes initialized')
+gq.printing = False
+# breakpoint()
 
-# Visualization
-make_movie = False
-
+# visualize
 if make_movie:
     ## Plotting during mesh refinement
     # fig, axs = plt.subplots(ncols=2, figsize=(6, 3), dpi=100)
     fig = plt.figure()
-    # axs = plt.gca(projection='3d')
     axs = plt.gca()
-    # axs.view_init(40,23)
     # parameters for animation
-    sweep_duration = 5#15
+    sweep_duration = 15
     hold_duration = 10
     total_duration = sweep_duration + hold_duration
-    fps = 5#15
+    fps = 30
 
     # setup animation writer
     import matplotlib.animation
     writer_class = matplotlib.animation.writers['ffmpeg']
     writer = writer_class(fps=fps, bitrate=1000)
-    writer.setup(fig, 'bubblewrap_2d_jpca.mp4')
+    writer.setup(fig, 'gqds_movie.mp4')
+else:
+    fig = plt.figure(figsize=(14,8))
+    trials = [5, 10, 25, 50, 100]
+    ts_to_plot = timesteps * np.array(trials)
+    ax_ind = 1
 
-## run online
-timer = time.time()
-times = []
-times_obs = []
+size = 10
 
-init = -M
-end = T-M
-step = batch_size
+com = center_mass(gq.mu)
 
-for i in np.arange(init, end, step):
-    # print(i)
+max_x = np.max(np.abs(gq.mu[:,0].flatten())) * 1.1
+max_y = np.max(np.abs(gq.mu[:,1].flatten())) * 1.1
+
+x = np.linspace(-max_x, max_x, size)
+y = np.linspace(-max_y, max_y, size)
+x, y = np.meshgrid(x, y)
+pos = np.dstack((x, y))
+
+## %% run online
+if ssSVD:
+    data_red_all = np.zeros((data.shape[0], k))
+    data_red_all[:max(M, k), :] = data_red
+else:
+    data_red_all = data
+
+
+for i in np.arange(-M, T - M):
     t1 = time.time()
-    gq.observe(data[i+M])
-    times_obs.append(time.time()-t1)
-    t2 = time.time()
-    gq.em_step()    
-    gq.grad_Q()
-    times.append(time.time()-t2)    
+    if ssSVD:
+        pro.updateSVD(data[i+M, np.newaxis].T)
+        data_red = pro.Q.T @ data[i+M].T
+        if data_red.sum() == 0:
+            breakpoint()
+        data_red_all[i+(2*M), :] = data_red
+        gq.observe(data_red)
+        gq.em_step()
+    else:
+        gq.observe(data[i+M])
+        gq.em_step()
+        for j in range(5):
+            gq.grad_Q()
+
+    if i % 75 == 0:
+        print(gq.pred[i+M])    
 
     if make_movie:
         if True: #i < 200 or i > 300:
-            # plots.plot3d_color(data[:i+1+M], t[:i+1+M], axs, alpha=1) #, cmap='PuBu')
-            axs.scatter(data[:i+1+M+step,0], data[:i+1+M+step,1], color='b', alpha=0.6)
+            plot_jPCA_trials(data, ax=axs)
             for n in np.arange(N):
-                if n in gq.dead_nodes: # or (gq.n_obs[n] < 0.08):
-            # #         ## don't plot dead nodes
+                if n in gq.dead_nodes:
+                    ## don't plot dead nodes
                     pass
                 else:
-                    el = np.linalg.inv(gq.L[n][:2,:2])
-                    sig = el.T @ el
-                    u,s,v = np.linalg.svd(sig)
-                    width, height = np.sqrt(s[0]*9), np.sqrt(s[1]*9) #*=4
-                    # if width>1e5 or height>1e5:
-                    #     pass
-                    # else:
-                    angle = atan2(v[0,1],v[0,0])*360 / (2*np.pi)
-                    # breakpoint()
-                    el = Ellipse((gq.mu[n,0],gq.mu[n,1]), width, height, angle, zorder=8)
-                    el.set_alpha(0.2)
-                    el.set_clip_box(axs.bbox)
-                    el.set_facecolor('r')  ##ed6713')
-                    axs.add_artist(el)
+                    # sig = gq.L[n] @ gq.L[n].T
+                    u,s,v = np.linalg.svd(gq.L[n])
+                    width, height = s[0]*2.25, s[1]*2.25 #*=4
+                    if width>1e5 or height>1e5:
+                        pass
+                    else:
+                        angle = atan2(v[0,1],v[0,0])*360 / (2*np.pi)
+                        # breakpoint()
+                        el = Ellipse((gq.mu[n,0],gq.mu[n,1]), width, height, angle, zorder=8)
+                        el.set_alpha(0.2)
+                        el.set_clip_box(axs.bbox)
+                        el.set_facecolor('r')  ##ed6713')
+                        axs.add_artist(el)
                 
-                    # axs.text(gq.mu[n,0]+0.1, gq.mu[n,1], gq.mu[n,2], s=str(n))
+                    # plt.text(gq.mu[n,0]+1, gq.mu[n,1], str(n))
 
-        # else: #i between 200 and 300
-        #     # find node closest to data point
-        #     node = np.argmax(gq.alpha)
-        #     A = gq.A.copy()
-        #     A[A<=(1/N)] = 0
-        #     plots.plot_color(data[:i+M+1, 0], data[:i+M+1, 1], t[:i+M+1], axs, alpha=1) #, cmap='PuBu')
-        #     for j in np.arange(N):
-        #         if A[node,j] > 0 and not node==j:
-        #             print('Arrow from ', str(node), ' to ', str(j))
-        #             axs.arrow(gq.mu[node,0], gq.mu[node,1], gq.mu[j,0]-gq.mu[node,0], gq.mu[j,1]-gq.mu[node,1], length_includes_head=True, width=A[node,j], head_width=0.8, color='k', zorder=9)
+        else: #i between 200 and 300
+            # find node closest to data point
+            node = np.argmax(gq.alpha)
+            A = gq.A.copy()
+            A[A<=(1/N)] = 0
+            plots.plot_color(data[:i+M+1, 0], data[:i+M+1, 1], t[:i+M+1], axs, alpha=1) #, cmap='PuBu')
+            for j in np.arange(N):
+                if A[node,j] > 0 and not node==j:
+                    print('Arrow from ', str(node), ' to ', str(j))
+                    axs.arrow(gq.mu[node,0], gq.mu[node,1], gq.mu[j,0]-gq.mu[node,0], gq.mu[j,1]-gq.mu[node,1], length_includes_head=True, width=A[node,j], head_width=0.8, color='k', zorder=9)
 
+        axs.scatter(gq.mu[:,0], gq.mu[:,1], c='k' , zorder=10)
+        plt.draw()
+        writer.grab_frame()
 
-            # plt.xlim([0,1600])
-            # plt.ylim([-25,15])
-            # axs.set_zlim([-1550,0])
-
-            mask = np.ones(gq.mu.shape[0], dtype=bool)
-            if gq.dead_nodes:
-                mask[np.array(gq.dead_nodes)] = False
-            # mask[gq.n_obs<1e-8] = False
-            # breakpoint()
-            axs.scatter(gq.mu[mask,0], gq.mu[mask,1], c='k' , zorder=10)
-
-            # axs.set_xticks([-15,15])
-            # axs.set_yticks([-15,20])
-            axs.set_xticks([])
-            axs.set_yticks([])
-
-            plt.draw()
+        if i >= 200 and i <= 300:
             writer.grab_frame()
+        
+        # axs[0].cla()
+        # axs[1].cla()
+        axs.cla()
+    else:
+        if i in ts_to_plot:
+            ax = fig.add_subplot(2, 3, ax_ind)
+            plot_jPCA_trials(i+M, timesteps, data_red_all, ax=ax)
+            draw_bubbles(gq, sig_ell=2, ax=ax)
+            # draw_graph(ax, gq, thresh=.0103)
+            # plot_jPCA_trials(data, ax=ax)
+            ax.set(title='trial {} ({:.2f} seconds)'.format(trials[ax_ind-1], i*.01))
+            ax_ind += 1
 
-            # if i >= 200 and i <= 300:
-            #     writer.grab_frame()
+xmin, ymin = data_red_all.min(axis=0)[:2]
+xmax, ymax = data_red_all.max(axis=0)[:2]
 
-            # if i+M in [M-1, floor(T/4)-1, floor(T/2)-1, T-1]:
-            #     figS = plt.gcf()
-            #     figS.savefig('vdp_'+str(i+1)+'.svg', bbox_inches='tight')
-            
-            # axs[0].cla()
-            # axs[1].cla()
-            axs.cla()
-
-
-    # print(i)
-    if (i+M) % 50 == 0  and i>0:
-        print(i+M, 'frames processed. Time elapsed ', time.time()-timer)
-
-
-print('Done fitting all data online')
-print('Average cycle time: ', np.mean(np.array(times)[20:]))
-print('Average observation time: ', np.mean(np.array(times_obs)[20:]))
-# print('Average prediction time: ', np.mean(np.array(gq.time_pred)[20:]))
-
-
+for currax in fig.axes:
+    currax.set(xlim=(xmin, xmax), ylim=(ymin, ymax))
 if make_movie:
     writer.finish()
 
+ax = fig.add_subplot(2, 3, ax_ind)
+ax.plot(gq.pred, color='grey', alpha=0.2)
+ax.plot(ewma(gq.pred, 100), color='black')
+ax.axvline(timesteps*100)
+ax.set(xlabel='timesteps', ylabel='log probability')
 
-plt.figure()
-plt.plot(np.array(gq.pred))
-var_tmp = np.convolve(np.array(gq.pred), np.ones(500)/500, mode='valid')
-plt.plot(var_tmp, 'k')
-# for tt in gq.teleported_times:
-#     plt.axvline(x=tt, color='r', lw=1)
+fig.suptitle('Bubblewrap approximates jPCA trajectories within ~10 trials', y=.95)
+fig.subplots_adjust(wspace=0.3)
+# plt.savefig('jpca_fig.pdf', bbox_inches='tight')
 
-# plt.show()
+#%% feed gq "new" trajectories - ORDER MATTERS!
+trials = 45, 27
 
+fig, ax = plt.subplots(1, 2, sharex=True, sharey=True, figsize=(9,4))
+for trial in trials:
+    start, end = int(timesteps*trial), int(timesteps*(trial+1))
+    newtrial = data[start:end, :]
+    closest_nodes = []
+    for i in range(newtrial.shape[0]):
+        gq.observe(newtrial[i])
+        gq.em_step()
+        closest_nodes.append(gq.current_node)
+    closest_nodes = np.array(closest_nodes, dtype='int')
+    nodes_to_bubble = np.unique(closest_nodes)
 
-plt.figure()
-axs = plt.gca()
-axs.scatter(data[:i+1+M+step,0], data[:i+1+M+step,1], color='m')
-# axs.plot(data[i+M+step-1:i+1+M+step,0], data[i+M+step-1:i+1+M+step,1], lw=2, color='b')
-for n in np.arange(N):
-    if n in gq.dead_nodes or (gq.n_obs[n] < 0.1):
-# #         ## don't plot dead nodes
-        pass
-    else:
-        el = np.linalg.inv(gq.L[n][:2,:2])
-        sig = el.T @ el
-        u,s,v = np.linalg.svd(sig)
-        width, height = np.sqrt(s[0]*9), np.sqrt(s[1]*9) #*=4
-        # if width>1e5 or height>1e5:
-        #     pass
-        # else:
-        angle = atan2(v[0,1],v[0,0])*360 / (2*np.pi)
-        # breakpoint()
-        el = Ellipse((gq.mu[n,0],gq.mu[n,1]), width, height, angle, zorder=8)
-        el.set_alpha(0.2)
-        el.set_clip_box(axs.bbox)
-        el.set_facecolor('r')  ##ed6713')
-        axs.add_artist(el)
-    
-        # axs.text(gq.mu[n,0]+0.01, gq.mu[n,1], s=str(n))
-mask = np.ones(gq.mu.shape[0], dtype=bool)
-if gq.dead_nodes:
-    mask[np.array(gq.dead_nodes)] = False
-axs.scatter(gq.mu[mask,0], gq.mu[mask,1], c='k' , zorder=10)
+    # plot 1 - all trials, all bubbles
+    draw_bubbles(gq, sig_ell=2, ax=ax[0])
+    plot_jPCA_trials(data.shape[0], timesteps, data, ax=ax[0], alpha=.03)
+    ax[0].plot(newtrial[:, 0], newtrial[:, 1], lw=2, color='blue')
+    # draw_graph(gq, ax=ax[0], thresh=1/N)
 
+    # plot 2 - one trial, closest bubbles, graph
+    A = np.copy(np.array(gq.A))
+    A_mask = np.ones(A.shape) # all true (to discard)
+    A_mask[nodes_to_bubble, :] = np.zeros(A_mask.shape[1]) # keep closest nodes
+    A_mask = A_mask.astype('bool')
+    A[A_mask] = 0
 
-plt.figure()
-axs = plt.gca()
-# axs.scatter(data[:i+1+M+step,0], data[:i+1+M+step,1], color='m')
-# axs.plot(data[i+M+step-1:i+1+M+step,0], data[i+M+step-1:i+1+M+step,1], lw=2, color='b')
-for n in np.arange(N):
-    if n in gq.dead_nodes or (gq.n_obs[n] < 0.1):
-# #         ## don't plot dead nodes
-        pass
-    else:
-        el = np.linalg.inv(gq.L[n][:2,:2])
-        sig = el.T @ el
-        u,s,v = np.linalg.svd(sig)
-        width, height = np.sqrt(s[0]*9), np.sqrt(s[1]*9) #*=4
-        # if width>1e5 or height>1e5:
-        #     pass
-        # else:
-        angle = atan2(v[0,1],v[0,0])*360 / (2*np.pi)
-        # breakpoint()
-        el = Ellipse((gq.mu[n,0],gq.mu[n,1]), width, height, angle, zorder=8)
-        el.set_alpha(0.2)
-        el.set_clip_box(axs.bbox)
-        el.set_facecolor('r')  ##ed6713')
-        axs.add_artist(el)
-    
-        # axs.text(gq.mu[n,0]+0.01, gq.mu[n,1], s=str(n))
+    draw_graph(gq, ax[1], A=A, thresh=1/N, node_color='orange')
+    draw_bubbles(gq, sig_ell=2, ax=ax[1], 
+                node_list=nodes_to_bubble)
+    plot_jPCA_trials(data.shape[0], timesteps, data, ax=ax[1], alpha=.03)
+    ax[1].plot(newtrial[:, 0], newtrial[:, 1], lw=2, color='blue')
+
+# %%
+
+# %%
+# datas, times = load_churchland_data(path)
+# num_jpcs = 2
+
+# jpca = jPCA.JPCA(num_jpcs=num_jpcs)
+# # Fit the jPCA object to data
+# (projected, 
+#  full_data_var,
+#  pca_var_capt,
+#  jpca_var_capt) = jpca.fit(datas, times=times, tstart=-50, tend=150)
+
+# data = np.array(projected).reshape((len(projected)*projected[0].shape[0], num_jpcs))
+# data = data[:T, :]
+# # data += np.random.randn(data.shape[0],data.shape[1])*0.05*np.std(data)
 
 
 
-plt.ylim([-5,5])
-plt.xlim([-5,5])
-
-plt.show()
-
-print('----------------')
-
-breakpoint()
+# %%
--- scripts/run_widefield.py
+++ scripts/run_widefield.py
@@ -9,22 +9,53 @@ from matplotlib.collections import LineCollection
 
 from field.gqds import GQDS
 from field.utils import center_mass
-from datagen.plots import draw_graph, draw_bubbles, plot_scatter_connected, plot_color
+from datagen.plots import draw_graph, draw_bubbles, plot_scatter_connected
 
+import mat73
 from math import atan2
 import umap
+from proSVD import proSVD
 
-#%% for smoothing log prob
+# %%
+def run_proSVD(data, k, l1, l, decay=1):
+    num_iters = np.ceil((data.shape[1] - l1) / l).astype('int')
+    print(num_iters)
+
+    pro = proSVD(k, history=num_iters, trueSVD=True)
+    pro.initialize(data[:, :l1])
+    print(pro.Qs.shape)
+
+    t = l1
+    for i in range(num_iters):
+        pro.updateSVD(data[:, t:t+l])
+        t += l
+
+    return pro
+
 def ewma(data, com):
     return np.array(pd.DataFrame(data=dict(data=data)).ewm(com).mean()['data'])
 
-#%% # load ssSVD trajectories
+#%% # load wfield
+matdict = mat73.loadmat('/hdd/pgupta/musall2019/Vc.mat')
+Sv = matdict['Sv']
+U = matdict['U']
+Vc = matdict['Vc']
+totalVar = matdict['totalVar']
 
-npz = np.load('/hdd/pgupta/stringer2019/neuropixel_reduced.npz')
-projed = npz['ssSVD10'].T
+comps, frames, trials = Vc.shape
+Vc = Vc.reshape((comps, frames*trials))
 
-total = projed.shape[0] # not all the timepoints
-data = projed[:total, :]
+total = frames * trials
+comps = 10
+data = (Sv[:comps, np.newaxis] * Vc[:comps, :total]).T
+# np.save('widefield_reduced.npy', data)
+#%% reduce further
+k = 6
+l1 = k
+l = 1
+pro = run_proSVD(data.T, k, l1, l)
+
+data = data @ pro.Q
 
 # %%time
 
@@ -33,7 +64,7 @@ make_movie = False
 
 # general params
 dt = 0.1
-M = 500 # 6 dims? # init size, one trial
+M = 100 # 6 dims? # init size, one trial
 T = total - M # iters
 t = np.linspace(0, dt*T, T)
 
@@ -46,6 +77,7 @@ nu = 1e-3
 step = 1e-3
 eps = 0
 
+# sigma_scale = 1e3
 B_thresh = -10
 n_thresh = 1e-5
 P = 0 
@@ -83,7 +115,7 @@ if make_movie:
     writer.setup(fig, 'gqds_movie.mp4')
 else:
     fig = plt.figure(figsize=(14,8))
-    ts_to_plot = [100, 500, 1500, 3000, T-M-3]
+    ts_to_plot = [100, 500, 1500, 5000, T-M-3]
     ax_ind = 1
 
 size = 10
@@ -113,7 +145,7 @@ for i in np.arange(-M, T - M - 1):
 
     if make_movie:
         if True: #i < 200 or i > 300:
-            plot_color(data[:i+1+M, 0], data[:i+1+M, 1], t[:i+1+M], axs, alpha=1) #, cmap='PuBu')
+            plots.plot_color(data[:i+1+M, 0], data[:i+1+M, 1], t[:i+1+M], axs, alpha=1) #, cmap='PuBu')
             for n in np.arange(N):
                 if n in gq.dead_nodes:
                     ## don't plot dead nodes
@@ -159,7 +191,7 @@ for i in np.arange(-M, T - M - 1):
     else:
         if i in ts_to_plot:
             ax = fig.add_subplot(2, 3, ax_ind)
-            draw_bubbles(gq, sig_ell=2, ax=ax)
+            draw_bubbles(gq, sig_ell=1, ax=ax)
             ax.set(xlim=(xmin, xmax), ylim=(ymin, ymax),
                    title='{} bins ({:.2f} seconds)'.format(i, i*.033))
             ax_ind += 1
@@ -180,14 +212,9 @@ ax.plot(gq.pred, color='grey', alpha=0.2)
 ax.plot(ewma(gq.pred, 100), color='black')
 ax.set(xlabel='timesteps', ylabel='log probability')
 
-# make up extra datapoints (for init)
-for i in range(M+1):
-    gq.observe(data[i])
-    gq.em_step()
-
 #%% run UMAP
 neighb = 5
-min_dist = .5
+min_dist = .05
 ufit = umap.UMAP(n_neighbors=neighb, min_dist=min_dist, 
                  n_components=2).fit(data)
 
@@ -195,8 +222,8 @@ data_umap = np.array([ufit.embedding_[:, 0], ufit.embedding_[:, 1]]).T
 mu_umap = ufit.transform(gq.mu)
 
 #%% get new trajectory
-trial = 1
-start, end = 1000, 1250
+trial = 10
+start, end = frames*trial, frames*(trial+1)
 newtrial = data[start:end, :]
 closest_nodes = []
 for i in range(newtrial.shape[0]):
@@ -264,26 +291,147 @@ axs[1,0].set_title('umap space, trajectories')
 axs[1,1].set_title('umap space,\ntop bubblewrap transitions')
 
 #%% log prob comparison
-vjf = np.load('models/logprob_vjf_neuropixel.npy')
-zp2016 = np.load('models/logprob_zp2016_neuropixel.npy')
+# make up extra datapoints (for init)
+for i in range(M+1):
+    gq.observe(data[i])
+    gq.em_step()
+
+vjf = np.load('models/logprob_vjf_widefield.npy')
+zp2016 = np.load('models/logprob_zp2016_widefield.npy')
 bwrap = np.array(gq.pred)
 
 plt.ioff()
 nn = vjf.shape[0]
 
 plt.plot(np.linspace(100, nn - 1, nn - 100), zp2016, color='red', alpha=0.2)
-plt.plot(vjf, color='grey', alpha=0.2)
+# plt.plot(vjf, color='grey', alpha=0.2)
 plt.plot(bwrap, color='lightblue', alpha=0.2)
 
 ax = plt.gca()
 ax.plot(np.linspace(100, nn - 1, nn - 100), ewma(zp2016, 100), color='red', label='ZP2016')
-ax.plot(ewma(vjf, 100), color='black', label='vjf')
+# ax.plot(ewma(vjf, 100), color='black', label='vjf')
 ax.plot(ewma(bwrap, 100), color='blue', label='bubblewrap')
 ax.ticklabel_format(useOffset=False, style='plain')
+
 
 ax.set_xlabel('time step')
 ax.set_ylabel('log probability')
 ax.set_title('predictive log probability at each time step')
 ax.legend()
-ax.set(ylim=(-30, 15))
+ax.set(ylim=(-30, 0))
+#%% 
+trial = 0
+start, end = frames*trial, frames*(trial+1)
+newtrial = data[start:end, :]
 
+closest_nodes = []
+for i in range(start, end):
+    gq.observe(newtrial[i])
+    gq.em_step()
+    closest_nodes.append(gq.current_node)
+    
+closest_nodes = np.array(closest_nodes, dtype='int')
+nodes_to_bubble = np.unique(closest_nodes)
+
+# plot 2 - one trial, closest bubbles, graph
+A = np.copy(np.array(gq.A))
+# np.fill_diagonal(A, 0) # 0 out diag (self weights don't count?)
+A_mask = np.zeros(A.shape)
+A_mask[nodes_to_bubble, :] = np.ones(A_mask.shape[1])
+A_mask = A_mask.astype('bool')
+# A_mask[A_mask < 1/N] = False
+A[A_mask == False] = 0
+
+#%% run UMAP
+neighb = 5
+min_dist = .5
+ufit = umap.UMAP(n_neighbors=neighb, min_dist=min_dist, 
+                 n_components=2).fit(data)
+
+data_umap = np.array([ufit.embedding_[:, 0], ufit.embedding_[:, 1]]).T
+mu_umap = ufit.transform(gq.mu)
+
+#%% plot 
+steps = data.shape[0]
+thresh = .09 # for gq.A, pretty arbitrary
+alpha_graph = 1
+alpha_data = 0.2 # for below graph
+
+# for svd and umap
+fig, axs = plt.subplots(2, 2, figsize=(12,12))
+fig.subplots_adjust(wspace=0.3, hspace=.1)
+
+# svd
+draw_graph(gq, axs[0, 1], A=A, thresh=thresh, alpha=alpha_graph) # svd space
+axs[0, 0].scatter(data[:steps, 0], data[:steps, 1], alpha=0.4, c=np.arange(steps))
+axs[0, 1].scatter(data[:steps, 0], data[:steps, 1], alpha=alpha_data,
+            c=np.arange(steps), zorder=0)
+draw_bubbles(N, gq, sig_ell=2, ax=axs[0,1], 
+            node_list=nodes_to_bubble)
+
+# umap
+draw_graph(gq, axs[1, 1], A=A, mu=mu_umap, thresh=thresh, alpha=alpha_graph)
+axs[1, 0].scatter(data_umap[:steps, 0], data_umap[:steps, 1], alpha=0.4, c=np.arange(steps))
+axs[1, 1].scatter(data_umap[:steps, 0], data_umap[:steps, 1], alpha=alpha_data,
+            c=np.arange(steps), zorder=0)
+
+axs[1,0].axis('off')
+axs[0,0].axis('off')
+
+axs[0,0].set_title('svd space, trajectories')
+axs[0,1].set_title('svd space,\ntop bubblewrap transitions')
+
+axs[1,0].set_title('umap space, trajectories')
+axs[1,1].set_title('umap space,\ntop bubblewrap transitions')
+
+
+
+#%% feed gq "new" trial, get closest nodes, A
+trial = 0
+start, end = frames*trial, frames*(trial+1)
+newtrial = data[start:end, :]
+
+closest_nodes = []
+for i in range(start, end):
+    gq.observe(newtrial[i])
+    gq.em_step()
+    closest_nodes.append(gq.current_node)
+    
+closest_nodes = np.array(closest_nodes, dtype='int')
+nodes_to_bubble = np.unique(closest_nodes)
+
+# plot 2 - one trial, closest bubbles, graph
+A = np.copy(np.array(gq.A))
+# np.fill_diagonal(A, 0) # 0 out diag (self weights don't count?)
+A_mask = np.zeros(A.shape)
+A_mask[nodes_to_bubble, :] = np.ones(A_mask.shape[1])
+A_mask = A_mask.astype('bool')
+# A_mask[A_mask < 1/N] = False
+A[A_mask == False] = 0
+
+draw_graph(gq, ax[1], A=A, thresh=1/N)
+draw_bubbles(N, gq, sig_ell=2, ax=ax[1], 
+            node_list=nodes_to_bubble)
+#%% plotting graph
+fig, ax = plt.subplots(1, 2, sharex=True, sharey=True, figsize=(9,4))
+
+# plot 1 - all trials, all bubbles
+draw_bubbles(N, gq, sig_ell=2, ax=ax[0])
+# plot_jPCA_trials(data.shape[0], timesteps, data, ax=ax[0])
+# draw_graph(gq, ax=ax[0], thresh=1/N)
+
+
+# %% check full low dim representation!
+
+# evil mouse, jpca,  (also in table) (in supplement: jpca bubbles over time, eigenspectrum, dim above/below )
+# panel for jpca, widefield, evil mouse
+# umap (for widefield) then dots/graphs
+# 
+# fig 3  
+
+# neuropixels umap graph, timing breakdown
+# timing for prediction and inference
+
+# evil mouse 
+# share this script with Anne
+# 
